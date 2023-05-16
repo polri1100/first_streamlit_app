83634432f3517cfe9ef0178fc059dd542ad94f7a
polri1100 <61285598+polri1100@users.noreply.github.com>
2023-05-16 16:59:45 +0200

    try block

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,15 +26,18 @@ streamlit.dataframe(fruits_to_show)
 
 #api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# normalize the data
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# outputs
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.write('The user entered ', fruit_choice)
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # normalize the data
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # outputs
+    streamlit.dataframe(fruityvice_normalized)
+except ULRError as e:
+  streamlit.error()
 
 streamlit.stop()
 
